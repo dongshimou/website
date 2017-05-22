98d5b07ae035502366da6081f4fa64fb4530a3fa
dongshimou <dongshimou@gmail.com>
2017-05-22 10:24:34 +0800

    move git repository

---
+++ src/components/Hello.vue
@@ -0,0 +1,104 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>Study Website</h2>
+    <ul>
+      <li v-for="item in Studyweb">
+        <a :href="item.a" target="_blank">{{item.name}}
+        </a>
+      </li>
+    </ul>
+    <h2>Ecosystem</h2>
+    <ul>
+      <li v-for="item in Ecosystem">
+        <a :href="item.a" target="_blank">{{item.name}}
+        </a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data() {
+    return {
+      msg: 'Welcome to My Vue.js App',
+      Ecosystem: [
+        {
+          name: 'vue-router',
+          a: 'http://router.vuejs.org/'
+        }, {
+          name: 'vuex',
+          a: 'http://vuex.vuejs.org/'
+        }, {
+          name: 'vue-loader',
+          a: 'http://vue-loader.vuejs.org/'
+        }, {
+          name: 'awesome-vue',
+          a: 'https://github.com/vuejs/awesome-vue'
+        }
+      ],
+      Studyweb: [
+        {
+          name: 'MDN',
+          a: 'https://developer.mozilla.org/zh-CN/'
+        },
+        {
+          name: 'W3C',
+          a: 'https://www.w3schools.com'
+        },
+        {
+          name: 'runjs',
+          a: 'http://runjs.cn/code'
+        }, {
+          name: 'jsfiddle',
+          a: 'https://jsfiddle.net/'
+        },
+        {
+          name: 'stackvoerflow',
+          a: 'https://stackoverflow.com'
+        }, {
+          name: 'github',
+          a: 'https://github.com'
+        }
+      ]
+
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 10px 10px;
+}
+
+a {
+  color: #000;
+  padding: 5px;
+  border-radius: 5px;
+  box-shadow: 0px 0px 1em #000;
+}
+
+li:hover {
+  transform: scale(1.1, 1.1);
+  transition: 0.3s;
+}
+
+.hello {
+  text-align: center;
+}
+</style>
---
+++ src/components/Music.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="fix_bottom">
+        <audio :src="src" autoplay controls loop>
+        </audio>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'music',
+    data() {
+        return {
+            music_list: [
+                '小时姑娘-爱殇.mp3'
+            ],
+            src: ''
+        }
+    },
+    created() {
+        let address = './music/'
+        let src = address + this.music_list[0]
+        this.src = src;
+        console.log(this.src)
+
+    },
+    mounted() {
+        const that = this
+        that.$el.firstElementChild.volume = 0.5
+        that.$el.style.position = 'fixed'
+        document.body.style.marginBottom = that.$el.clientHeight + 'px';
+    }
+}
+</script>
+
+<style>
+audio {
+    width: 100%;
+}
+
+.fix_bottom {
+    width: 100%;
+    bottom: 0px;
+    display: grid;
+}
+</style>
